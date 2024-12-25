a0eb95514eb3663a85e3b9a356ff6d51cf4651d3
DREAM <voigkgky@gmail.com>
2024-12-25 21:34:05 +0700

    Save Loind then..

--- src/router/routet.ts
+++ src/router/routet.ts
@@ -1,9 +1,9 @@
 import express from 'express'
-import { users_Update, users_Loing, get_token_01} from '../_connext_api/Users'
+import { users_Update, users_Loing} from '../_connext_api/Users'
 const app = express.Router()
 
 app.put('/users/:id', users_Update)
 app.post('/Loing', users_Loing)
-app.get('/token', get_token_01)
+// app.get('/token', get_token_01)
 
 export default app
